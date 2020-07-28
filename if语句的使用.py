dea25b516431b2eea2d98f110dbe1a3c730c1e27
1558017468@qq.com <1558017468@qq.com>
2020-07-28 16:52:13 +0800

    选择结构if

---
+++ if语句的使用.py
@@ -0,0 +1,11 @@
+"""
+登录验证
+
+"""
+username = input('请输入用户的姓名：')
+password = input('请输入密码：')
+# 用户名是admin且密码是123456则身份验证成功否则身份验证失败
+if username == 'abc' and password == '123456':
+    print('successful')
+else:
+    print('false')
