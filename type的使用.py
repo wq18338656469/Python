2ca2f32b8d6631536db04893408993bf745dffde
1558017468@qq.com <1558017468@qq.com>
2020-07-28 11:12:54 +0800

    python基础学习_type的使用

---
+++ type的使用.py
@@ -0,0 +1,11 @@
+"""type检查变量的类型"""
+a = 200
+b = 12.988
+c = 1 + 5j
+d = 'hello world'
+e = False
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'float'>
+print(type(c)) # <class 'complex'>
+print(type(d)) # <class 'str'>
+print(type(e)) # <class 'bool'>
