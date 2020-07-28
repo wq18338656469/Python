011cd49c685bdf6900332be93298caf1fa1819c7
wqcode <61272201+wq18338656469@users.noreply.github.com>
2020-07-28 16:54:32 +0800

    Update if语句的使用.py

--- if语句的使用.py
+++ if语句的使用.py
@@ -4,7 +4,7 @@
 """
 username = input('请输入用户的姓名：')
 password = input('请输入密码：')
-# 用户名是admin且密码是123456则身份验证成功否则身份验证失败
+# 用户名是abc且密码是123456则身份验证成功否则身份验证失败
 if username == 'abc' and password == '123456':
     print('successful')
 else:
