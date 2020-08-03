2f19ba4ab608cc0cc1a06511c2916fc9b6cb1e61
1558017468@qq.com <1558017468@qq.com>
2020-08-03 10:58:27 +0800

    Python基础_元组练习

---
+++ 元组练习.py
@@ -0,0 +1,28 @@
+"""
+下面的代码演示了如何定义和使用元组。
+"""
+# 定义元组
+t = ('wang', 25, True, '河南郑州')
+print(t)
+# 获取元组中的元素
+print(t[0])
+print(t[3])
+# 遍历元组中的值
+for member in t:
+    print(member)
+# 重新给元组赋值
+# t[0] = '张三'  # TypeError
+# 变量t重新引用了新的元组原来的元组将被垃圾回收
+t = ('张三', 20, True, '云南昆明')
+print(t)
+# 将元组转换成列表
+person = list(t)
+print(person)
+# 列表是可以修改它的元素的
+person[0] = '李小龙'
+person[1] = 25
+print(person)
+# 将列表转换成元组
+fruits_list = ['APP', 'Baidu', 'Jingdong']
+fruits_tuple = tuple(fruits_list)
+print(fruits_tuple)
